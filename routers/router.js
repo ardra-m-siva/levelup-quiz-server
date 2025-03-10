438680d49d94287472a80d6b34fa09505b6144ee
Ardra m s <ardrasivan2000@gmail.com>
2025-03-10 22:23:30 +0530

    fetching from api added

--- routers/router.js
+++ routers/router.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/userController')
 const jwtMiddleware = require('../middlewares/jwtMiddleware')
 const multerMiddleware = require('../middlewares/multerMiddleware')
 const router = express.Router()
+const axios=require('axios')
 
 router.post('/register', userController.registerUserController)
 
@@ -10,5 +11,22 @@ router.post('/login', userController.loginUserController)
 
 //  jwt middleware is not used anywhere now 
 router.put('/update', jwtMiddleware,multerMiddleware.single("profilePic"), userController.updateUserController)
+// api to fetch the questions
+
+router.get('/api/questions',async (req, res) => {
+    try {
+        const { difficulty, subject } = req.query;
+        const response = await axios.get(`https://quizapi.io/api/v1/questions`, {
+            params: { limit: 10, difficulty, subject },
+            headers: {
+                "Content-Type": "application/json",
+                "X-Api-Key": process.env.API_KEY  // Hidden from frontend
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching questions", error: error.message });
+    }
+})
 
 module.exports = router
