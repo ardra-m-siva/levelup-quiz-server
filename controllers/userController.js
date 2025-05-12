73879df5b84a2d0629fe98a2c63caba350dc2ff8
Ardra m s <ardrasivan2000@gmail.com>
2025-05-12 20:18:16 +0530

    user validation added

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,13 +22,18 @@ exports.registerUserController = async (req, res) => {
 exports.loginUserController = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const existingUser = await users.findOne({ email, password })
-        if (existingUser) {
-            const token = jwt.sign({ userid: existingUser._id }, process.env.JWTPASSWORD)
-            res.status(200).json({ user: existingUser, token })
-        } else {
-            res.status(401).json("Incorrect Email/ Password")
+        const existingUser = await users.findOne({ email })
+        if (!existingUser) {
+            res.status(401).json("Invalid Email.")
+        }else{
+            if (existingUser.password!==password) {
+                return res.status(401).json("Invalid password.");
+            }else{
+                const token = jwt.sign({ userid: existingUser._id }, process.env.JWTPASSWORD)
+                res.status(200).json({ user: existingUser, token })
+            }
         }
+
     } catch (err) {
         res.status(404).json("Login Failed")
     }
